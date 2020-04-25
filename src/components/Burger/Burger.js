a4857a8886c3a60580b80a6af50b5fd6420b460b
Marcelo Borges Pereira <marcelo.borges@inatel.br>
2020-04-25 12:01:53 -0300

    Handle scenario of no ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,16 +3,24 @@ import classes from './Burger.module.css';
 import Ingredient from './Ingredient/Ingredient';
 
 const burger = props => {
-  const ingredients = Object.keys(props.ingredients).map(key => {
-    return [...Array(props.ingredients[key])].map((_, i) => (
-      <Ingredient key={key + i} type={key} />
-    ));
-  });
+  const ingredients = Object.keys(props.ingredients)
+    .map(key => {
+      return [...Array(props.ingredients[key])].map((_, i) => (
+        <Ingredient key={key + i} type={key} />
+      ));
+    })
+    .reduce((arr, element) => {
+      return [...arr, ...element];
+    }, []);
 
   return (
     <div className={classes.Burger}>
       <Ingredient type="bread-top" />
-      {ingredients}
+      {ingredients.length ? (
+        ingredients
+      ) : (
+        <p>Please start adding ingredients</p>
+      )}
       <Ingredient type="bread-bottom" />
     </div>
   );
