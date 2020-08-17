fb0cff973cabe4a2fe3266c40180509d26073277
Marcelo Borges Pereira <marcelo.borges@inatel.br>
2020-08-17 13:20:40 +0000

    Fix App test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders empty burger', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Please start adding ingredients/i);
   expect(linkElement).toBeInTheDocument();
 });
